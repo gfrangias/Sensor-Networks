1146e5d2d75caa89a3c04c4b38d71ba06102b17b
Georgios Frangias <georgios.frangias@outlook.com>
2022-10-18 22:18:30 +0300

    Create helpful_program.py
    A program that outputs the relations between sensors in a sensor matrix of DxD dimensions.

---
+++ helpful_program.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from cmath import sqrt
+import math
+import sys
+
+D = int(sys.argv[1])    # Dimension of the DxD sensor matrix
+n = float(sys.argv[2])  # Distance between neighboring sensors
+r = float(sys.argv[3])  # Range
+
+for i in range(0, D**2):
+    i_long = (i // D) * n
+    i_lat = (i % D) * n
+    for j in range (0, D**2):
+        if i!=j:
+            j_long = (j // D) * n
+            j_lat = (j % D) * n
+            # Euclidean distance
+            dist = math.sqrt((i_long-j_long)**2 +(i_lat-j_lat)**2)
+            # Check if the distance is in range
+            if dist <= r:
+                print(i," ",j," -50.0")
