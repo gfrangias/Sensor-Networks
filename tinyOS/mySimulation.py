58ccbd6bf233a8575495921fb7f16da17d360758
Georgios Frangias <georgios.frangias@outlook.com>
2022-10-25 14:17:26 +0300

    Ran the simulation for the first time
    Also updated topology_creator.py (aka helpful_program.py) to be in line with the exercise specs.
    Starting to add comments in files.

--- tinyOS/mySimulation.py
+++ tinyOS/mySimulation.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
+####### Create TOSSIM Object ####### 
 from TOSSIM import *
 import sys ,os
 import random
 
 t=Tossim([])
 f=sys.stdout #open('./logfile.txt','w')
+####################################
+
+# Compute when the simulation ended
 SIM_END_TIME= 1000 * t.ticksPerSecond()
 
 print "TicksPerSecond : ", t.ticksPerSecond(),"\n"
 
+####### Select which debug messages are shown on terminal #######
 t.addChannel("Boot",f)
 t.addChannel("RoutingMsg",f)
 t.addChannel("NotifyParentMsg",f)
@@ -17,14 +22,18 @@ t.addChannel("Radio",f)
 #t.addChannel("Serial",f)
 t.addChannel("SRTreeC",f)
 #t.addChannel("PacketQueueC",f)
+#################################################################
 
+####### Start nodes in range, at slightly different moments #######
 for i in range(0,10):
 	m=t.getNode(i)
 	m.bootAtTime(10*t.ticksPerSecond() + i)
+###################################################################
 
-
+# Choose which topology file to open 
 topo = open("topology.txt", "r")
 
+# When topology file can't be found
 if topo is None:
 	print "Topology file not opened!!! \n"
 
--- topology_creator.py
+++ topology_creator.py
@@ -5,19 +5,18 @@ import math
 import sys
 
 D = int(sys.argv[1])    # Dimension of the DxD sensor matrix
-n = float(sys.argv[2])  # Distance between neighboring sensors
-r = float(sys.argv[3])  # Range
+r = float(sys.argv[2])  # Range
 
 with open("topology.txt",'r+') as f:
     f.truncate(0)
 
 for i in range(0, D**2):
-    i_long = (i // D) * n
-    i_lat = (i % D) * n
+    i_long = (i // D) 
+    i_lat = (i % D)
     for j in range (0, D**2):
         if i!=j:
-            j_long = (j // D) * n
-            j_lat = (j % D) * n
+            j_long = (j // D) 
+            j_lat = (j % D)
             # Euclidean distance
             dist = math.sqrt((i_long-j_long)**2 +(i_lat-j_lat)**2)
             # Check if the distance is in range
